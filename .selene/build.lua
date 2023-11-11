0d8f991835a6cda24cd2ccdda31facac930443f1
Frank Mayer <gpg@frankmayer.dev>
2023-11-11 02:42:32 +0100

    added project file

--- .selene/build.lua
+++ .selene/build.lua
@@ -1,3 +1,7 @@
-local bin_name = selene_os_type() == "windows" and "bloodmage_game.exe" or "bloodmage_game"
+local pack = require("pack")
+local bin_name = pack.bin_name
+if Selene.os_type() == "windows" then
+	bin_name = bin_name .. ".exe"
+end
 
-os.execute("go build -o " .. bin_name .. " game/main.go")
+os.execute('go build -ldflags="-s -w" -o \'' .. bin_name .. "' game/main.go")
---
+++ .selene/install.lua
@@ -0,0 +1,2 @@
+os.execute("go mod tidy")
+
